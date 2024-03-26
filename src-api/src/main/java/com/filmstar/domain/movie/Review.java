bf8c91a343504f94198181091efb4f5c40fbf93a
Pablo Calvo <pablocalvoj@gmail.com>
2024-03-26 23:30:33 +0100

    Adding id to Review and implementing getters/setters

--- src-api/src/main/java/com/filmstar/domain/movie/Review.java
+++ src-api/src/main/java/com/filmstar/domain/movie/Review.java
@@ -2,9 +2,35 @@ package com.filmstar.domain.movie;
 
 public class Review {
 	
+	private Long id;
+	
 	private String comment;
+	
+	public Review() {
+		
+	}
 
 	public Review(String comment) {
 		this.comment = comment;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
+	
 }
