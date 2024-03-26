64712f6042c473f71544736685ae22052886cf38
Pablo Calvo <pablocalvoj@gmail.com>
2024-03-27 00:33:02 +0100

    UserAuthRequest and ReviewMoviePostRequest

---
+++ src-api/src/main/java/com/filmstar/apps/movierecords/ReviewMoviePostRequest.java
@@ -0,0 +1,17 @@
+package com.filmstar.apps.movierecords;
+
+public class ReviewMoviePostRequest {
+	
+	public String comment;
+	
+	public ReviewMoviePostRequest() {
+		
+	}
+
+	public ReviewMoviePostRequest(String comment) {
+		this.comment = comment;
+	}
+	
+	
+
+}
