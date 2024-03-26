452039f40a8b4302552361019cb9bb1515d01309
Pablo Calvo <pablocalvoj@gmail.com>
2024-03-26 23:24:26 +0100

    Email value object class

---
+++ src-api/src/main/java/com/filmstar/domain/user/Email.java
@@ -0,0 +1,31 @@
+package com.filmstar.domain.user;
+import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class Email implements Serializable {
+
+    private String value;
+
+    public Email() {
+    }
+
+    public Email(String value) throws IllegalArgumentException{
+        ensureEmailIsValid(value);
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    private void ensureEmailIsValid(String value) throws IllegalArgumentException {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(value);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Email address is not valid");
+        }
+    }
+}
