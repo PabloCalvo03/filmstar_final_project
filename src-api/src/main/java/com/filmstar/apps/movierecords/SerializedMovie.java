26dc8d236cb72b83e9c0caafed934653fcc6d643
Pablo Calvo <pablocalvoj@gmail.com>
2024-03-26 23:31:53 +0100

    Adding PosterImg and Reviews to SerializedMovie class

--- src-api/src/main/java/com/filmstar/apps/movierecords/SerializedMovie.java
+++ src-api/src/main/java/com/filmstar/apps/movierecords/SerializedMovie.java
@@ -2,6 +2,12 @@ package com.filmstar.apps.movierecords;
 
 import com.filmstar.apps.backoffice.SerializedDirector;
 import com.filmstar.domain.movie.Movie;
+import com.filmstar.domain.movie.Review;
+import com.filmstar.domain.movie.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class SerializedMovie {
 	
@@ -9,19 +15,32 @@ public class SerializedMovie {
     public String title;
     public String overview;
 
+	public String posterImg;
+	public List<Review> reviews;
+
 	public SerializedDirector director;
     
-    public SerializedMovie(String id, String title, String overview, SerializedDirector movieDirector) {
+    public SerializedMovie(String id, String title, String overview, List<Review> reviews, String posterImg,
+						   SerializedDirector movieDirector) {
 		this.id = id;
 		this.title = title;
 		this.overview = overview;
+		this.posterImg = posterImg;
+		this.reviews = reviews;
 		this.director = movieDirector;
 	}
 
 	public static SerializedMovie from(Movie movie) {
-    	return new SerializedMovie(movie.id().value(), movie.title().value(), movie.overview().value(),
-				SerializedDirector.from(movie.director()));
-    }
+		List<Review> reviews = movie.reviews() != null ? movie.reviews() : new ArrayList<>();
+		return new SerializedMovie(
+				movie.id().value(),
+				movie.title().value(),
+				movie.overview().value(),
+				reviews,
+				movie.posterImg().value(),
+				SerializedDirector.from(movie.director())
+		);
+	}
 
 
 }
