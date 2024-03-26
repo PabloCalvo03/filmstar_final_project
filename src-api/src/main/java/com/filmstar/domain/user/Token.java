b50855214d9faad4679be0d9b68284cf06834aa4
Pablo Calvo <pablocalvoj@gmail.com>
2024-03-27 00:31:45 +0100

    Token class

---
+++ src-api/src/main/java/com/filmstar/domain/user/Token.java
@@ -0,0 +1,18 @@
+package com.filmstar.domain.user;
+
+public class Token {
+	
+private String value;
+	
+	public Token() {
+    }
+	
+	public Token(String value) {
+		this.value = value;
+	}
+
+	public String value() {
+		return value;
+	}
+
+}
