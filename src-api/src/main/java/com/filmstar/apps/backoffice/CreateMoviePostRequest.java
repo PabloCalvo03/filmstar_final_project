12a1b948e8d5e83ec2614836ec22b52e09fe4eea
Pablo Calvo <pablocalvoj@gmail.com>
2024-03-26 23:28:50 +0100

    Adding posterImg to CreateMoviePostRequest

--- src-api/src/main/java/com/filmstar/apps/backoffice/CreateMoviePostRequest.java
+++ src-api/src/main/java/com/filmstar/apps/backoffice/CreateMoviePostRequest.java
@@ -7,16 +7,19 @@ public class CreateMoviePostRequest implements Serializable{
 	public String id;
     public String title;
     public String description;
+
+    public String posterImg;
     public String directorId;
     
     public CreateMoviePostRequest() {
     	
     }
     
-    public CreateMoviePostRequest(String id, String title, String description, String directorId) {
+    public CreateMoviePostRequest(String id, String title, String description, String posterImg, String directorId) {
     	this.id = id;
     	this.title = title;
     	this.description = description;
+        this.posterImg = posterImg;
         this.directorId = directorId;
     }
 
