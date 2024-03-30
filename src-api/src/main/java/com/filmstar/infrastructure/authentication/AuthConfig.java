7c7aad2d5cb3b2f8d96707c3a5212d2f3466bc02
Pablo Calvo <pablocalvoj@gmail.com>
2024-03-30 03:33:11 +0100

    moving AuthConfig to authentication

--- src-api/src/main/java/com/filmstar/infrastructure/authentication/AuthConfig.java
+++ src-api/src/main/java/com/filmstar/infrastructure/authentication/AuthConfig.java
@@ -1,4 +1,4 @@
-package com.filmstar;
+package com.filmstar.infrastructure.authentication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
