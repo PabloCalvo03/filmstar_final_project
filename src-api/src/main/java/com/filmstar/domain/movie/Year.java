7b7e9b83aeaa9b84aaebd47c6f5af0b4d5b70b1b
PabloCalvo03 <pablocalvoj@gmail.com>
2024-06-01 16:19:19 +0200

    adding Year class

---
+++ src-api/src/main/java/com/filmstar/domain/movie/Year.java
@@ -0,0 +1,15 @@
+package com.filmstar.domain.movie;
+
+public class Year {
+
+    private Integer value;
+
+    public Year(Integer year){
+
+    }
+
+    public Integer value(){
+        return value;
+    }
+
+}
